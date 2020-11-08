3b76e048ff0b62ad33b40de639385dced45871e6
AndrewChen <chenpoanandrew@gmail.com>
2020-11-08 16:08:20 +0800

    feat(build.sh): change to use git tag as image tag.

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-COMMIT_ID=$(git rev-parse --verify HEAD)
-IMAGE_NAME=andrewchen20/mockingbird:$COMMIT_ID
+# COMMIT_ID=$(git rev-parse --verify HEAD)
+TAG=$(git tag --points-at HEAD)
+IMAGE_NAME=andrewchen20/mockingbird:$TAG
 
 sudo docker-compose run --rm build
 sudo docker-compose build --force-rm mockingbird
